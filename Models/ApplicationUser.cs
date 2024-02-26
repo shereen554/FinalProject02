66ffc0379359b9676699d00aa9991db7485628c3
Shereen Ahmed <84847116+shereen554@users.noreply.github.com>
2024-02-26 22:39:57 +0200

    Update ApplicationUser.cs

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -5,7 +5,6 @@ namespace WebApplication1.Models
     public class ApplicationUser:IdentityUser
     {
         public byte[]? Image { get; set; }
-        public string pharmDoctor { get; set; } = null!;
-        public int UPharmacyId { get; set; }
+       
     }
 }
